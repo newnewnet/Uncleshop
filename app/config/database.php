6d171e27ac4621536e07bb986ffdadcd586a9c31
unknown <newnewnas@hotmail.com>
2014-10-07 23:58:30 +0700

    config database

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'uncloshop',
+			'database'  => 'uncleshop',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
