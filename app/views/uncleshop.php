6b45fa1b31b2d6b55cd6a243670bb07e991b55b1
apisit <apisitjadi@hotmail.com>
2014-10-09 00:40:02 +0700

    include less complete

--- app/views/uncleshop.php
+++ app/views/uncleshop.php
@@ -3,11 +3,17 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Upcleshop</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	    <script src="js/bootstrap.min.js"></script>
+
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.4/angular.min.js"></script>
-		
+
 		<link rel="stylesheet/less" type="text/css" href="css/styles.less" />
-		
-		<!-- <link rel="stylesheet" href="css/main.css"> -->
 
 		<script src="js/ng.js"></script>
 		<!-- set options before less.js script -->
@@ -29,7 +35,10 @@
 	</head>
 
 	<body ng-controller="uncleshopController">
-		<div >		
-		</div>
+
+		<button type="button" class="btn btn-danger">
+			Action <span class="caret"></span>
+		</button>
+
 	</body>
 </html>
