6d2953bacea864a031bc51b0932da8e08981bc36
apisit <apisitjadi@hotmail.com>
2014-10-09 00:19:23 +0700

    please include the less and bootstrap

--- app/views/uncleshop.php
+++ app/views/uncleshop.php
@@ -4,9 +4,28 @@
 		<meta charset="UTF-8">
 		<title>Upcleshop</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.4/angular.min.js"></script>
-		<script src="js/ng.js"></script>
 		
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet/less" type="text/css" href="css/styles.less" />
+		
+		<!-- <link rel="stylesheet" href="css/main.css"> -->
+
+		<script src="js/ng.js"></script>
+		<!-- set options before less.js script -->
+		<script>
+		  less = {
+		    env: "development",
+		    async: false,
+		    fileAsync: false,
+		    poll: 1000,
+		    functions: {},
+		    dumpLineNumbers: "comments",
+		    relativeUrls: false,
+		    rootpath: ":/a.com/"
+		  };
+		</script>
+		<script src="js/less/less.js" type="text/javascript"></script>
+		<!--script src="less.js"></script-->
+	
 	</head>
 
 	<body ng-controller="uncleshopController">
