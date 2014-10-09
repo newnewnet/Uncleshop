e0b22e6b1021efc47fe1398befd434d26a0427b2
apisit <apisitjadi@hotmail.com>
2014-10-09 22:54:52 +0700

    slide

--- app/views/uncleshop.php
+++ app/views/uncleshop.php
@@ -2,7 +2,13 @@
 <html lang="en" ng-app="uncleshopApp">
 	<head>
 		<meta charset="UTF-8">
-		<title>Upcleshop</title>
+		<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<title>Uncleshop</title>
+
+		<!-- Angularjs -->
+		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+		<!-- <script src="https://code.angularjs.org/1.2.8/angular-animate.min.js"></script> -->
 
 		<!-- Bootstrap -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
@@ -11,11 +17,16 @@
 	    <!-- Include all compiled plugins (below), or include individual files as needed -->
 	    <script src="js/bootstrap.min.js"></script>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.4/angular.min.js"></script>
+	    <!-- Google Font -->
+	    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+
+		<!-- Javascript File -->
+		<script src="js/ng.js"></script>
+
+		<!-- Font Awesome -->
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
 		<link rel="stylesheet/less" type="text/css" href="css/styles.less" />
-
-		<script src="js/ng.js"></script>
 		<!-- set options before less.js script -->
 		<script>
 		  less = {
@@ -36,9 +47,18 @@
 
 	<body ng-controller="uncleshopController">
 
-		<button type="button" class="btn btn-danger">
-			Action <span class="caret"></span>
-		</button>
+		<div class="container-fluid COLOR-BG">
+			<div class="container-top-label FLOAT-LEFT TEXT-LEFT">
+				<i class="fa fa-bars" ng-click="WarpTabSlide()"></i>
+			</div>
+			<div class="container-top-label FLOAT-LEFT TEXT-RIGHT">
+				Nuttapon
+			</div>
+		</div>
+
+		<div class="warp_tab_hide" ng-class="{warp_tab_show: WarpTab}">
+			asddf
+		</div>
 
 	</body>
 </html>
--- public/js/ng.js
+++ public/js/ng.js
@@ -1,10 +1,25 @@
 angular.module('uncleshopApp', [])
 .controller('uncleshopController', ['$scope','bill', function($scope,bill) 
 {
-	bill.getBill(function(data, status, headers, config)
-	{
-		console.log(data);
-	});
+	$scope.WarpTab = false;
+	$scope.WarpTabSlide = function(){
+		$scope.WarpTab = !$scope.WarpTab;
+		console.log($scope.WarpTab);
+	};
+
+	// $scope.WarpTab = 'warp-tab-hide';
+	// $scope.WarpTabSlide = function(){
+	// 	if($scope.WarpTab == 'warp-tab-hide')
+	// 		$scope.WarpTab = 'warp-tab-show';
+	// 	else
+	// 		$scope.WarpTab = 'warp-tab-hide';
+	// 	$scope.$apply();
+	// 	console.log($scope.WarpTab);
+	// };
+	// bill.getBill(function(data, status, headers, config)
+	// {
+	// 	console.log(data);
+	// });
 }])
 .factory('bill', ['$http', function($http) 
 {
