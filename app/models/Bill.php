a7444761e9a6da46101e405fd44b2f1438e07127
unknown <newnewnas@hotmail.com>
2014-10-22 00:20:50 +0700

    api bill

--- app/models/Bill.php
+++ app/models/Bill.php
@@ -10,10 +10,10 @@
 		{
 			$customers  = new Customers;
 			$product = new Product;
+			$bill_detail = new Bill_detail;
 
 			if(isset($array['cutomers_name']))
-			{
-				
+			{			
 				$customers->insert([
 					'customers_id' => $array['customers_id'],
 					'customers_name' => $array['cutomers_name'],
@@ -23,64 +23,67 @@
 				]);
 			}
 
+			// $billCode = $this->randomBill();
 
-			$billCode = $this->randomBill();
-
-		// 	$this->insert([
-		// 			'bill_id' => $billCode ,
-		// 			'bill_start_date' => date('Y-m-d'),
-		// 			'bill_end_date' => $array['customers_id'],
-		// 			'bill_total_price' => $array['customers_id'],
-		// 			'bill_date_amount' => $array['customers_id'],
-		// 			'bill_status' => $array['customers_id'],
-		// 			'bill_price' => $array['customers_id'],
-		// 			'bill_type' => $array['customers_id'],
-		// 			'customers_id' => $array['customers_id'],
-		// 			'admin_id' => $array['customers_id'],
-		// ]);
-			// $expire_start = new DateTime();
-
+			// $day = 0;
+			// $month = date('m');
+			// $year   = date('Y');
 
 			// if($array['bill_type'] == 1)
 			// {
-
+			// 	$day = 15*$array['bill_date_amount'];
+			// 	$endDate = time() + ($day *24*60 * 60);
+			// 	$endDate  = date('Y-m-d',$endDate );
 			// }
-			// else
+			// else 
 			// {
-			// 	$array['bill_type']
-			// 	$expireDate = time() + ( 3 *24*60 * 60);
-			// 	$expireDate  = date('Y.m.d',$expireDate );
-			// }
+			// 	for($i=0 ;$i<$array['bill_date_amount'];$i++)
+			// 	{
+			// 			if($month == 12)
+			// 			{
+			// 				$month = 1;
+			// 			}
+			// 			else if($i != 0)
+			// 			{
+			// 				$month+=1;
+			// 			}
 			
-			// echo $expireDate ;
+			// 			$day+=$this->daysInMonth($month,$year);
+			// 			$expireDate = time() + ($day *24*60 * 60);
+			// 			$year = date('Y',$expireDate);					
+			// 	}
+			// 	$endDate = time() + ($day *24*60 * 60);
+			// 	$endDate  = date('Y-m-d',$endDate );
+			// }
 
-			$day = 0;
-			$month = date('m');
+			// $this->insert([
+			// 		'bill_id' => $billCode ,
+			// 		'bill_start_date' => date('Y-m-d'),
+			// 		'bill_end_date' => $endDate,
+			// 		'bill_total_price' => $array['bill_total_price'],
+			// 		'bill_date_amount' => $array['bill_date_amount'],
+			// 		'bill_status' => 0,
+			// 		'bill_price' => $array['bill_price'],
+			// 		'bill_type' => $array['bill_type'],
+			// 		'bill_price_dow' => $array['bill_price_dow'],
+			// 		'customers_id' => $array['customers_id'],
+			// 		'admin_id' => $array['admin_id'],
+			// ]);
 
-			$year   = date('Y');
-
-			for($i=0 ;$i<$array['bill_date_amount'];$i++)
-			{
-				$day+=$this->daysInMonth($month+$i,$year);
-
-				$expireDate = time() + ($day *24*60 * 60);
-				$year = date('Y',$expireDate );
-				// echo $year;
-			}
-			$endDate = time() + ($day *24*60 * 60);
-			$endDate  = date('Y.m.d',$endDate );
-			echo $day;
-
-			// echo date('Y-m-d');
-    	// echo $billCode;
+			// foreach ( $array['product'] as $product)
+			// {
+			// 	$product->insert([
+			// 			'product_name' => $product->product_name,
+			// 			'product_amount' => $product->product_amount,
+			// 			'product_price' => $product->product_price,
+			// 			'bill_id' => $billCode
+			// 	]);
+			// }
 
 
 		}
-
-
 		private function daysInMonth($month, $year)
 		{
-		// calculate number of days in a month
 			return $month == 2 ? ($year % 4 ? 28 : ($year % 100 ? 29 : ($year % 400 ? 28 : 29))) : (($month - 1) % 7 % 2 ? 30 : 31);
 		}
 		private function randomBill()
